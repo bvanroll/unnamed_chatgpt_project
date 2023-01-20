b21dd9b590354fa10d2eac0e3aef27600e6d889c
Beppe Vanrolleghem <beppe.vanrolleghem@gmail.Com>
2023-01-20 13:43:54 +0100

    Delete main/target directory

--- main/target/debug/deps/unnamed_chatgpt_project-a697a553acbc9a7c.d
+++
@@ -1,5 +0,0 @@
-/Users/beppe/Git/unnamed_chatgpt_project/main/target/debug/deps/unnamed_chatgpt_project-a697a553acbc9a7c.rmeta: src/main.rs
-
-/Users/beppe/Git/unnamed_chatgpt_project/main/target/debug/deps/unnamed_chatgpt_project-a697a553acbc9a7c.d: src/main.rs
-
-src/main.rs:
--- main/target/debug/deps/unnamed_chatgpt_project-db02dcd8b2c85351.d
+++
@@ -1,5 +0,0 @@
-/Users/beppe/Git/unnamed_chatgpt_project/main/target/debug/deps/unnamed_chatgpt_project-db02dcd8b2c85351.rmeta: src/main.rs
-
-/Users/beppe/Git/unnamed_chatgpt_project/main/target/debug/deps/unnamed_chatgpt_project-db02dcd8b2c85351.d: src/main.rs
-
-src/main.rs:
