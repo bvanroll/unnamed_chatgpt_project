1b3eff9f8814ccc2591370a3196c1a33d5fb2b42
bvanroll <beppe.vanrolleghem@gmail.com>
2023-01-20 13:43:22 +0100

    project start

---
+++ main/target/debug/deps/unnamed_chatgpt_project-a697a553acbc9a7c.d
@@ -0,0 +1,5 @@
+/Users/beppe/Git/unnamed_chatgpt_project/main/target/debug/deps/unnamed_chatgpt_project-a697a553acbc9a7c.rmeta: src/main.rs
+
+/Users/beppe/Git/unnamed_chatgpt_project/main/target/debug/deps/unnamed_chatgpt_project-a697a553acbc9a7c.d: src/main.rs
+
+src/main.rs:
---
+++ main/target/debug/deps/unnamed_chatgpt_project-db02dcd8b2c85351.d
@@ -0,0 +1,5 @@
+/Users/beppe/Git/unnamed_chatgpt_project/main/target/debug/deps/unnamed_chatgpt_project-db02dcd8b2c85351.rmeta: src/main.rs
+
+/Users/beppe/Git/unnamed_chatgpt_project/main/target/debug/deps/unnamed_chatgpt_project-db02dcd8b2c85351.d: src/main.rs
+
+src/main.rs:
